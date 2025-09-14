f1a0442c9cec16757cc229b0d91d66aeb372b204
Nageri Ibrahim <ibrahimdamilola43@gmail.com>
2025-09-14 16:31:26 +0100

    !Spam Commit

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,33 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    public bool spawned = false;
+    public float delay;
 
     // Update is called once per frame
     void Update()
     {
+        // Reset delay
+        Reset();
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && !spawned)
         {
+            delay = 0.5f;
+            spawned = true;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+        }
+    }
+
+    private void Reset()
+    {
+        if (spawned && delay > 0f)
+        {
+            delay -= Time.deltaTime;
+        }
+        if (delay < 0f)
+        {
+            delay = 0f;
+            spawned = false;
         }
     }
 }
